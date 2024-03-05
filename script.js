596696f8cf531c3f25389483d21731257a2055b0
DimaDamage <korshuns@iMac-Korshuns.local>
2024-03-05 15:19:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sendbutton = document.getElementById('sendbutton');
+    
+    sendbutton.addEventListener('click', function () {
+        alert("Your message has been sent!");
+    });
+});
+
