353dc0eaaee87212f9c9c098d6083ef28282e675
DimaDamage <korshuns@iMac-Korshuns.local>
2024-03-06 17:11:55 +0200

    11 commit

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sendbutton = document.getElementById('sendbutton');
-    
-    sendbutton.addEventListener('click', function () {
-        alert("Your message has been sent!");
-    });
-});
 
+/*Проверяем форму, если все заполнено отправляет*/
+
+document.getElementById('sendbutton').addEventListener('click', function() {
+    var name = document.getElementById('nname').value;
+    var email = document.getElementById('mail').value;
+    var phone = document.getElementById('pphone').value;
+    var message = document.getElementById('iinfo').value;
+
+    if (name === '' || email === '' || phone === '' || message === '') {
+        alert('Please fill in all fields.');
+    } else {
+        document.getElementById('myForm').submit();
+        alert('Your message has been sent!');
+    }
+});
